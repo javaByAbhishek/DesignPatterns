99c9a496ecb2fd12bdaaa2cbfc0d1cef29162679
Abhishek <abhishek1101209@gmail.com>
2024-10-10 17:36:41 +0530

    initial commit

---
+++ src/main/java/com/abhishek/AbhishekApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbhishekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbhishekApplication.class, args);
+	}
+
+}
